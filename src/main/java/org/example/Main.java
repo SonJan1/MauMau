3c6dc2aaa766cec6cc055f9a6af49da570c7b9df
Yan <gorez9999@gmail.com>
2024-06-18 21:52:49 +0200

    add card, deck, enums.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Card card = new Card(Rank.ACE,Suit.DIAMOND);
+        System.out.println(card);
     }
 }
---
+++ src/main/java/org/example/Rank.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public enum Rank {
+    ACE("Ass"),
+    KING("König"),
+    QUEEN("Dame"),
+    JACK("Bube"),
+    TEN("10"),
+    NINE("9"),
+    EIGHT("8"),
+    SEVEN("7");
+
+    private final String name;
+
+    Rank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
