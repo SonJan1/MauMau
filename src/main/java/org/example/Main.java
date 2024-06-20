c0e1731b6450559d4326adb3807893042ca2273c
Yan <gorez9999@gmail.com>
2024-06-20 23:10:20 +0200

    remove commented code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,8 @@ package org.example;
 
 import org.example.interfaces.Game;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
-//        Deck deck = new Deck();
-//        Set<Card> cardSet = new HashSet<>(deck.cards);
-//
-//        if (cardSet.size() == deck.size()) {
-//            System.out.println("Das Deck ist korrekt initialisiert, es gibt keine Duplikate.");
-//        } else {
-//            System.out.println("Duplikate im Deck erkannt!");
-//        }
-//
-//        System.out.println("Alle Karten im Deck:");
-//        for (Card card : deck.cards) {
-//            System.out.println(card);
-//        }
-
         String[] playerNames = {"Player 1", "Player 2", "Player 3"};
         boolean[] isHumanPlayers = {true, false, false};
         Game game = new GameImpl(playerNames, isHumanPlayers);
